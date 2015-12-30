1c888e42a153e4ee6edfa03c9db0426486c81dde
fidlkm <fidlkm@ufl.edu>
2015-12-30 16:16:28 -0500

    Added a new pic and made it display instead of default.

--- core/src/com/superschmalgames/DemoGame.java
+++ core/src/com/superschmalgames/DemoGame.java
@@ -13,7 +13,7 @@ public class DemoGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("kyloren.jpg");
 	}
 
 	@Override
