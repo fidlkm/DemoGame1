f9c9b39b67573586e4e0b0091058158585cf6116
Kristopher Fidler <Kristopher Fidler>
2016-01-03 18:43:50 -0500

    Added a basic background image over which our sprite traverses. Used LibGDX's camera tool to handle rendering the background image. Still need to revisit tutorials to learn more about LibGDX viewports and automatic handling of different resolutions.

--- core/src/com/superschmalgames/DemoGame.java
+++ core/src/com/superschmalgames/DemoGame.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
@@ -17,12 +18,15 @@ public class DemoGame extends ApplicationAdapter {
 	Texture img1, img2;
 
 	//Declare sprites to be used in the game.
-	Sprite sprite1;
+	Sprite sprite1, sprite2;
 
 	//Declare fonts to be used in the game.
 	BitmapFont font1;
 	String titleText;
 	GlyphLayout layout1;  //The layout variable is used to format and align font text within the rendered window.
+
+	//Create camera used to "view" stuff.
+	OrthographicCamera camera;
 
 	//The create() method gets called once, just to initialize variables and set everything up.
 	@Override
@@ -42,10 +46,15 @@ public class DemoGame extends ApplicationAdapter {
 		img2 = new Texture("badlogic.jpg");
 
 		sprite1 = new Sprite(img1);  //Set the visuals for the sprite to be whatever is in img2.
+		sprite2 = new Sprite(new Texture("house.png"));   //Set texture for house in viewport.
 
 		//Sprites have their own fields. You can set their position fields like so:
 		sprite1.setPosition(Gdx.graphics.getWidth()/2-sprite1.getWidth()/2,
 							Gdx.graphics.getHeight()/2-sprite1.getHeight()/2);
+
+		//Set up the camera to view our game world.
+		camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		camera.translate(camera.viewportWidth/2, camera.viewportHeight/2);  //Move camera to look at middle of our game background.
 	}
 
 	//The render() method is what's called every time the screen refreshes.
@@ -53,14 +62,22 @@ public class DemoGame extends ApplicationAdapter {
 	public void render() {
 
 		//Take keyboard input to move the sprite around the screen. Uses polling (instead of event-driven) to look for input.
-		if(Gdx.input.isKeyPressed(Input.Keys.LEFT))
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
 			sprite1.translateX(-1f);
-		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+			camera.translate(-1f,0f);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
 			sprite1.translateX(1f);
-		if(Gdx.input.isKeyPressed(Input.Keys.DOWN))
+			camera.translate(1f,0f);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
 			sprite1.translateY(-1f);
-		if(Gdx.input.isKeyPressed(Input.Keys.UP))
+			camera.translate(0f,-1f);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.UP)) {
 			sprite1.translateY(1f);
+			camera.translate(0f,1f);
+		}
 
 		//Take mouse input to move the sprite around the screen. Coordinate systems are different between the LibGDX
 		//coordinate plane and standard UI coordinate plane, so mathematical correction for Y-axis was need in order
@@ -73,8 +90,15 @@ public class DemoGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 0, 0, 1);   //Arguments are RGB values and Opacity, all between 0 and 1.
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+		//Update our camera so that the translation we did above will render properly.
+		camera.update();
+
 		//Whatever is drawn comes between the "begin" and "end" statements. (Verify this is necessary!)
 		batch.begin();
+
+		//Draw the ugly house background which is loaded into the sprite2 variable.
+		batch.setProjectionMatrix(camera.combined);
+		sprite2.draw(batch);
 
 		//Two different methods for drawing something to the screen. One uses just a raw image, the other uses
 		//a sprite which already contains its own positional data. *Be sure to only use one or the other for now!*
@@ -102,5 +126,6 @@ public class DemoGame extends ApplicationAdapter {
 	public void dispose(){
 		img1.dispose();
 		img2.dispose();
+		sprite2.getTexture().dispose();
 	}
 }
